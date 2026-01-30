d527d567daecdbf4ec69a4951627e5bce2a7e162
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-01-30 09:59:19 +0530

    feat: update project structure and implement chess game functionality
    - Changed module type from CommonJS to ES Module in package.json
    - Added dependencies for chess.js, http, socket.io, and nodemon
    - Created backend.yml to outline server-side logic for chess game
    - Created frontend.yml to detail client-side interactions and game setup
    - Added initial EJS template for the home page

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('this is the index.js file');
+import express from 'express';
+import { Socket } from 'socket.io';
+import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Chess } from 'chess.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const server = http.createServer(app);
+const io = Socket(server);
+const chess = new Chess();
+
+app.use(express.static(path.join(__dirname,"public")));
+app.set('view engine','ejs');
+
+const players = {}
+const currentPlayer = 'w';
